64f79ce0b6529713f5933b3081a23cfd6b81ee70
ilia <ilia@uui.ventures>
2019-08-22 13:55:08 +0300

    basic CRUD for ClassRoom entity

---
+++ src/Entity/ClassRoom.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\ClassRoomRepository")
+ */
+class ClassRoom
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Type("string")
+     * @Assert\Length(min=2, max=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @Assert\DateTime()
+     * @Assert\NotBlank()
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="boolean")
+     * @Assert\Type("boolean")
+     * @Assert\NotNull()
+     */
+    private $is_active;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getIsActive(): ?bool
+    {
+        return $this->is_active;
+    }
+
+    public function setIsActive(bool $is_active): self
+    {
+        $this->is_active = $is_active;
+
+        return $this;
+    }
+}
